1f7b9c6915a8d1d1ef0741f7dcc40f705fff5a4b
Vikram Naik <26vikramnaik@gmail.com>
2024-11-08 18:22:18 +0530

    feat(route): impl routing using react router dom

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,32 @@
+import { Routes, Route, Outlet } from "react-router-dom"
+
 import { LoginForm } from '@/components/LoginForm'
+import Dashboard from "@/pages/Dashboard";
 
 function App() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-2">
-      <LoginForm />
-    </main>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="login" element={<LoginForm />} />
+
+        {/* <Route path="*" element={<NoMatch />} /> */}
+      </Route>
+    </Routes>
   )
 }
 
+function Layout() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-2">
+      <header>
+      // TODO: add navbar
+      </header>
+
+      <Outlet />
+    </main>
+  );
+}
+
 export default App
+
